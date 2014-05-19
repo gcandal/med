0e06d12c35aeba1081000950fa026bf52de6f0a4
Gabriel Candal <gabrielcandal@gmail.com>
2014-05-19 23:06:53 +0100

    Missing files added.

---
+++ actions/procedures/deleteentitypayer.php
@@ -0,0 +1,21 @@
+<?php
+include_once('../../config/init.php');
+include_once($BASE_DIR . 'database/procedures.php');
+
+if (!$_SESSION['email']) {
+    $_SESSION['error_messages'][] = 'Tem que fazer login';
+    header('Location: ' . $BASE_URL);
+
+    exit;
+}
+
+$accountId = $_SESSION['idaccount'];
+$identitypayer = $_POST['identitypayer'];
+
+if(!deleteEntityPayer($accountId, $identitypayer)) {
+    $_SESSION['error_messages'][] = 'Esta entidade não pode ser apagada porque está associada a um procedimento';
+}
+
+header("Location: $BASE_URL" . "pages/procedures/payers.php");
+
+?>
